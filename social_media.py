29be30fb2ac871fa559b944d96358ce6101dfd6d
yaqoobgmt26@gmail.com <97838251+yaqoobgmt@users.noreply.github.com>
2026-02-01 11:07:38 +0530

    signup and signin added

--- social_media.py
+++ social_media.py
@@ -4,6 +4,7 @@ class chatting:
         self.password = ''
         self.loggedin = False
         self.menu()
+        self.users = []
         
         
     def menu(self):
@@ -16,14 +17,35 @@ class chatting:
                             """)
         
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
             pass
         else:
             exit()
+    
+    def signup(self):
+        email = input("enter your email-> ")
+        password = input("enter your password-> ")
+        self.username = email
+        self.password = password
+        print("Success")
+        print("\n")
+        self.menu()
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("Please SignUp first !")
+        else:
+            uname = input("enter your username or email ->")
+            pwd = input("enter your password ->")
+            if self.username==uname and self.password==pwd:
+                print("Success !!")
+                self.loggedin = True
+            else:
+                print("Please input correct credentials")
+        
         
 obj = chatting()
